53162f69ae4ccb068c03575626f7f820c3b207e3
shauryachichra5 <shauryachichra@gmail.com>
2025-09-04 14:09:19 +0530

    Create bot_reply.py
    > Service for bot-reply to user

---
+++ app/services/bot_reply.py
@@ -0,0 +1,16 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from functions.extract_topics import extract_topics
+import traceback
+
+def bot_reply(user_query: str):
+    try:
+        response, images = extract_topics(user_query)
+    except Exception as e:
+        response = "Sorry chatbot offline it seems!😓"
+        print(f"Error occured")
+        traceback.print_exc()
+
+    return response
