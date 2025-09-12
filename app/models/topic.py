9362144b38458ac6eae78ba7f0d82c3fdce0fa15
shauryachichra5 <shauryachichra@gmail.com>
2025-09-12 14:01:01 +0530

    Update topic.py

--- app/models/topic.py
+++ app/models/topic.py
@@ -4,7 +4,8 @@ from typing import List
 class TopicList(BaseModel):
     topic_list: List[str] = Field(
         description="List of topics that can be used to search for images catered to the user's needs",
-        example="[mountains, hills]"
+        example="[mountains, hills, cool monutain peaks]",
+        min_items = 4
     )
     exists: bool = Field(
         description="True if the query is talking about searching images, False if just general greeting"
